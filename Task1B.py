e33d9f5a42a3e400d020bb4f221d016c8550012a
e-merican <ecm69@cam.ac.uk>
2022-02-08 11:39:11 +0000

    debug

--- Task1B.py
+++ Task1B.py
@@ -8,12 +8,12 @@ def run():
     station_towns = {}
     for station in stations:
         station_towns[station.name] = station.town
-    list_stations_by_distance = [(station.name, station_towns[station.name], haversine(p, station.coord)) for station in stations]
+    stations_and_towns_by_distance = [(station, station_towns[station], dist) for (station,dist) in list_stations_by_distance]
 
     print("Closest 10")
-    print(list_stations_by_distance[:10])
+    print(stations_and_towns_by_distance[:10])
     print("Furthest 10")
-    print(list_stations_by_distance[-10:])
+    print(stations_and_towns_by_distance[-10:])
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
