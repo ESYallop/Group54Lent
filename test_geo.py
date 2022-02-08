78ae940db979294ee43e10a2348c4f50e4214b7b
ESYallop <ey257@cam.ac.uk>
2022-02-08 23:36:03 +0000

    initial attempt at test functions

--- test_geo.py
+++ test_geo.py
@@ -1,7 +1,7 @@
 """Unit test for the geo module"""
 
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import rivers_with_station, stations_by_river, rivers_by_station_number
+from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, rivers_by_station_number, stations_within_radius, haversine
 
 def test_rivers_with_station():
     #build list of stations and rivers
@@ -41,3 +41,24 @@ def test_rivers_by_station_number():
     stations_along_river = stations_by_river(stations)
     station_numbers_along_river = [len(station) for river, station in stations_along_river.items()]
     assert top_7_rivers[0] == max(station_numbers_along_river)
+
+
+def test_stations_by_distance(stations, p):
+    #build list of stations and sorted by haversine distance
+    stations = build_station_list()
+    test_stations_by_distance = stations_by_distance(stations, (52.2053, 0.1218))
+    #check that list is sorted properly
+    assert all(test_stations_by_distance[i] >= test_stations_by_distance[i + 1] for i in range(len(test_stations_by_distance)-1))
+
+
+def test_stations_within_radius(stations, centre, r):
+    #build list of stations
+    stations = build_station_list()
+    #check how many stations are within the radius
+    radius_station_number = 0
+    for station in stations:
+        if haversine(station, r) < 10:
+            radius_station_number +=1
+    #compare with number given by function
+    stations_and_towns_by_distance = stations_by_distance(stations)
+    assert radius_station_number == len(stations_by_distance)
