77ee5fe8dffba293d71bb0c39c33ff9222c7c43b
e-merican <ecm69@cam.ac.uk>
2022-02-27 19:41:38 +0000

    debug

--- test_plot.py
+++ test_plot.py
@@ -2,7 +2,7 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_levels
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+import datetime
 from floodsystem.datafetcher import fetch_measure_levels
 
 def test_plot_water_levels():
